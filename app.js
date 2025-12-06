ff5cb6a30a00b6593b1202e6d6857c4bd46cf262
Sunny Ranpise <sunnyranpise04@gmail.com>
2025-12-06 14:07:09 +0530

    Added a new files

---
+++ app.js
@@ -0,0 +1,23 @@
+// What is merge conflict?
+
+// -> A merge conflict happens when two branches change the same part of a file, and Git doesn’t know which change is correct.
+
+// Let's understand Merge Conflict by an example.
+
+
+// Pull command 
+
+// Pull command is used to get the latest changes from GitHub into your local computer repository.
+
+// syntax : git pull
+
+
+// Forking
+
+// Forking means creating your own copy of someone else’s GitHub repository into your own GitHub account.
+
+// What is the difference between forking and clone command?
+
+// Fork → creates a copy on GitHub.
+
+// Clone → downloads a repository to your computer.
